dc34858aabd74720c05a71d1698cf3be0d37573a
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-06-01 18:19:18 +0200

    Another attempt at fixing sphinx

--- content/conf.py
+++ content/conf.py
@@ -92,13 +92,13 @@ html_theme_options = {
         "notebook_interface": "jupyterlab",
     },
 }
-html_sidebars = {
-    "**": [ "sidebar-logo.html", "sbt-sidebar-nav.html", "sbt-sidebar-footer.html"]
-}
+#html_sidebars = {
+#    "**": [ "navbar-logo.html", "sbt-sidebar-nav.html", "sbt-sidebar-footer.html"]
+#}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static", "inputs"]
+html_static_path = ["_static",]
 # html_css_files = ["overrides.css"]
 # HTML context:
 from os.path import basename, dirname, realpath
